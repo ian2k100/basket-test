86c6c33102194cd7af1cdabdf078822486f74b75
ian2k100 <ianrmukoma@gmail.com>
2022-01-28 02:10:17 +0000

    First Form of the basket package, this folder needs a handful of packages(npx create-react-app, npm install react-use-cart, install react-bootstrap bootstrap@5.1.3). if the program still doesn't work after installing these packages then you might need to do ((npm uninstall -g create-react-app) then (rm -rf /usr/local/bin/create-react-app) in the bash terminal. I'll try to integrate this into our project.

---
+++ src/data.js
@@ -0,0 +1,31 @@
+import breadcat from './images/breadcat.jpg';
+import funnycat from './images/funnycat.jpg';
+import selfiecat from './images/selfiecat.jpg';
+
+const data = {
+    productData:[
+        {
+            id: 1,
+            img: breadcat,
+            title: 'Bread Cat',
+            desc: '',
+            price: 46,
+        },
+        {
+            id: 2,
+            img: funnycat,
+            title: 'Funny Cat',
+            desc: '',
+            price: 50,
+        },
+        {
+            id: 3,
+            img: selfiecat,
+            title: 'Selfie Cat',
+            desc: '',
+            price: 60,
+        }
+    ],
+};
+
+export default data;
